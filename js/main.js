52663aba423f587aee5f5fe604674f48a00d3a65
taviking <wiking1317@gmail.com>
2021-09-21 09:00:08 +0300

    create: Added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  autoHeight: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--right',
+    prevEl: '.slider-button--left',
+  },
+
+keyboard: {
+  enabled: true,
+  onlyInViewport: true,
+  pageUpDown: true,
+},
+});
