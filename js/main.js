2dfc7c0a7f6ba77f277a192c51bb3bd06edbb5ce
taviking <wiking1317@gmail.com>
2021-10-02 15:40:41 +0300

    create: modal section & added functionality

--- js/main.js
+++ js/main.js
@@ -51,3 +51,33 @@ menuButton.addEventListener('click', function () {
   console.log("клик по кнопке меню");
   document.querySelector(".navbar-menu").classList.toggle('navbar-menu--visible');
 });
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal)
+
+function openModal(){
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+
+  $(document).on('keydown', function (e) {
+    if (e.keyCode == 27) {
+      var modalOverlay = $('.modal__overlay');
+      var modalDialog = $('.modal__dialog');
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    }
+  });
+
+
+function closeModal(event){
+  event.preventDefault();
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
